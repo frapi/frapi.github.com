85bacdefa613303819d6e2d7c0826fc8a4ea7692
David Coallier <david@echolibre.com>
2010-05-31 12:11:20 +0100

    Commited what I forgot to commit earlier. Added a footer with copyright to CC-By

--- js/script.js
+++ js/script.js
@@ -1,14 +1,16 @@
 // Thanks janl and nslater
 // hosted scripts
+
 scripts = [
-    "http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js",
     "http://www.google-analytics.com/ga.js",
+    "http://code.jquery.com/jquery-latest.min.js"
 ]
 
 // load hosted scripts
 for(i = 0; i < scripts.length; i++) {
     document.write("<script src='" + scripts[i] + "'></script>");
 }
+
 
 // run after hosted scripts have loaded
 document.onready = function() {
@@ -18,10 +20,10 @@ document.onready = function() {
         $("*/[id!='']").wrap(function() {
             return "<a class='anchor' href='#" + $(this).attr("id") + "' />";
         });
+
         // activate analytics tracking
         _gat._getTracker("UA-XXXXX-Y")._trackPageview();
     } catch (error) {
         // uh oh, better have a debug console handy
     }
 }
-
